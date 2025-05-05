65247146547069bda2879242024c5a50ac96feee
alishaikh9890 <alishanshaikh9890@gmail.com>
2025-05-05 15:00:45 +0530

    props branchsetting

--- src/App.jsx
+++ src/App.jsx
@@ -6,29 +6,8 @@ import Navbar from './components/Navbar'
 function App() {
 
   return (
-    <frameElement>
-      <Navbar/>
-      <div className="max-w-7xl mx-auto border p-4">
-        <div className="flex gap-2 flex-wrap ">
-          <div className="shadow shadow-lime-400 rounded-md p-5 w-[25%]">
-            first Box
-          </div>
-          <div className="shadow shadow-lime-400 rounded-md p-5 w-[25%]">
-            first Box
-          </div>
-          <div className="shadow shadow-lime-400 rounded-md p-5 w-[25%]">
-            first Box
-          </div>
-          <div className="shadow shadow-lime-400 rounded-md p-5 w-[25%]">
-            first Box
-          </div>
-          <div className="shadow shadow-lime-400 rounded-md p-5 w-[25%]">
-            first Box
-          </div>
-        </div>
-      </div>
-      <Footer/>
-    </frameElement>
+    <>
+    </>
   )
 }
 
