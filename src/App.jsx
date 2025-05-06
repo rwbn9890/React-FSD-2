f50edde378c6c51160642c944896f72a0d89a427
alishaikh9890 <alishanshaikh9890@gmail.com>
2025-05-06 15:12:59 +0530

    passing props

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,29 @@
 
 import './App.css'
-import Footer from './components/Footer'
+import Card from './components/Card'
 import Navbar from './components/Navbar'
+import Products from './components/Products'
+
+import products from "./utilities/data"
+
 
 function App() {
 
+  const logo = {
+    img:"https://flowbite.com/docs/images/logo.svg",
+    text: "flowbite"
+  }
+
+
   return (
     <>
+
+<Navbar  logo={logo} len={products.length}/>
+    <Products data={products}/>
+
+
+<h1 >big</h1>
+
     </>
   )
 }
