3af0182d1315e4bb60ae65191575c0328b6e923c
alishaikh9890 <alishanshaikh9890@gmail.com>
2025-05-21 14:57:02 +0530

    intro routering

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
 import Card from './components/Card'
 import Navbar from './components/Navbar'
@@ -7,6 +7,11 @@ import Products from './components/Products'
 
 import products from "./utilities/data"
 import Cart from './components/Cart'
+import { BrowserRouter, Route, Router, Routes } from 'react-router'
+import Home from './Pages/Home'
+import Contact from './Pages/Contact'
+import Product from './Pages/Product'
+import About from './Pages/About'
 
 
 function App() {
@@ -17,18 +22,42 @@ function App() {
     img:"https://flowbite.com/docs/images/logo.svg",
     text: "flowbite"
   }
+  
+  
+  function setLocal(newCart){
+    localStorage.setItem("cart", JSON.stringify(newCart))
+    getCart()
+  }
 
+  function getCart(){
+   const newCart = JSON.parse(localStorage.getItem("cart"))
+
+   setCart(newCart)
+  }
+
+
+
+  useEffect(()=>{
+    getCart()
+  }, [])
 
   function addCart(obj){
 
-    let newObj = cart.find((ele) => ele.title == obj.title)
-    if(newObj){
-    let newCart= cart.map((ele) => ele.title == newObj.title ? {...ele, qtn:ele.qtn++} : ele)
-    setCart(newCart)
-    }
-    else{
-      setCart([...cart, {...obj, qtn:1}])
-    }
+
+    let newCart = cart;
+
+    newCart.push(obj)
+    // let newObj = cart.find((ele) => ele.title == obj.title)
+    // if(newObj){
+    // let newCart= cart.map((ele) => ele.title == newObj.title ? {...ele, qtn:ele.qtn++} : ele)
+    // setCart(newCart)
+    // }
+    // else{
+    //   setCart([...cart, {...obj, qtn:1}])
+    // }
+
+    setLocal(newCart)
+
   }
   console.log(cart)
 
@@ -36,9 +65,24 @@ function App() {
   return (
     <>
 
-<Cart cart={cart}/>
+    {/* <Products data={products} addCart={addCart}/>
+
+<Cart cart={cart}/> */}
+
+<BrowserRouter>
 <Navbar  logo={logo} len={cart.length}/>
-    <Products data={products} addCart={addCart}/>
+
+
+
+  <Routes>
+    <Route path="/" element={<Home/>}/>
+    <Route path="/contact" element={<Contact/>}/>
+    <Route path="/product" element={<Product  data={products} addCart={addCart}/>}/>
+    <Route path="/about" element={<About/>}/>
+    <Route path="/cart" element={<Cart  cart={cart}/>}/>
+  </Routes>
+
+</BrowserRouter>
 
 
 <h1 >big</h1>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router'
 
 const Navbar = ({len, logo}) => {
   return (
@@ -17,25 +18,37 @@ const Navbar = ({len, logo}) => {
     <div className="hidden w-full md:block md:w-auto" id="navbar-default">
       <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
         <li>
-          <a href="#" className="block py-2 px-3 text-white bg-blue-700 rounded-sm md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500" aria-current="page">Home</a>
+          <Link to='/'>
+          <a className="block py-2 px-3 text-white bg-blue-700 rounded-sm md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500" aria-current="page">Home</a>
+          </Link>
         </li>
         <li>
-          <a href="#" className="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">About</a>
+          <Link to='/about'>
+          <a className="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">About</a>
+          </Link>
         </li>
         <li>
-          <a href="#" className="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Services</a>
+          <Link to='/product'>
+          <a className="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Product</a>
+          </Link>
         </li>
         <li>
-          <a href="#" className="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Pricing</a>
+          <Link to='/'>
+          <a className="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Pricing</a>
+          </Link>
         </li>
         <li>
-          <a href="#" className="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Contact</a>
+          <Link to='/contact'>
+          <a className="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Contact</a>
+          </Link>
         </li>
         <li>
-          <a href="#" className="bg-yellow-400 text-black block py-1 px-4 rounded-sm ">
+          <Link to="/cart">
+          <a className="bg-yellow-400 text-black block py-1 px-4 rounded-sm ">
             Cart 
             <span className="bg-white text-black ms-3 rounded-2xl p-1" >{len}</span> 
             </a>
+          </Link>
         </li>
       </ul>
     </div>
