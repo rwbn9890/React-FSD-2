b47b1fe3c13250c1d94b6dc7d6812c934f000754
alishaikh9890 <alishanshaikh9890@gmail.com>
2025-05-26 15:24:47 +0530

    routing repeat

--- src/App.jsx
+++ src/App.jsx
@@ -1,105 +1,32 @@
 
 import { useEffect, useState } from 'react'
-import './App.css'
-import Card from './components/Card'
-import Navbar from './components/Navbar'
-import Products from './components/Products'
-
-import products from "./utilities/data"
-import Cart from './components/Cart'
-import { BrowserRouter, Route, Router, Routes } from 'react-router'
-import Home from './Pages/Home'
-import Contact from './Pages/Contact'
-import Product from './Pages/Product'
-import About from './Pages/About'
-import Login from './Pages/Login'
-
+import NavbarExample from './components/Navbar'
+import Home from './components/Home'
+import About from './components/About'
+import Users from './components/Users'
+import Contact from './components/Contact'
+import { Routes, Route } from 'react-router'
+import User from './components/User'
 
 function App() {
 
-  const [cart, setCart] = useState([])
-  const [auth, setAuth] = useState({})
-
-  const logo = {
-    img:"https://flowbite.com/docs/images/logo.svg",
-    text: "flowbite"
-  }
   
-  
-  function setLocal(newCart){
-    localStorage.setItem("cart", JSON.stringify(newCart))
-    getCart()
-  }
-
-  function getCart(){
-   const newCart = JSON.parse(localStorage.getItem("cart"))
-
-   setCart(newCart)
-  }
-
-
-
-  useEffect(()=>{
-    getCart()
-  }, [])
-
-
-  useEffect(()=>{
-    let newAuth = JSON.parse(localStorage.getItem("auth"))
-    setAuth(newAuth)
-  }, [])
-
-  function addCart(obj){
-
-
-    let newCart = cart;
-
-    newCart.push(obj)
-    // let newObj = cart.find((ele) => ele.title == obj.title)
-    // if(newObj){
-    // let newCart= cart.map((ele) => ele.title == newObj.title ? {...ele, qtn:ele.qtn++} : ele)
-    // setCart(newCart)
-    // }
-    // else{
-    //   setCart([...cart, {...obj, qtn:1}])
-    // }
-
-    setLocal(newCart)
-
-  }
-  console.log(cart)
-
 
   return (
     <>
 
-    {/* <Products data={products} addCart={addCart}/>
+<NavbarExample/>
 
-<Cart cart={cart}/> */}
-
-<BrowserRouter>
-{
-
-    auth &&  <Navbar  logo={logo} len={cart.length} setAuth={setAuth}/>
-}
-
-
-
-  <Routes>
-    <Route path="/" element={<Login  setAuth={setAuth}/>}/>
-    <Route path="/Home" element={<Home/>}/>
-    <Route path="/contact" element={<Contact/>}/>
-    <Route path="/product" element={<Product  data={products} addCart={addCart}/>}/>
-    <Route path="/about" element={<About/>}/>
-    <Route path="/cart" element={<Cart  cart={cart}/>}/>
-  </Routes>
-
-</BrowserRouter>
+<Routes>
+  <Route path="/" element={ <Home/>} />
+  <Route path="/about" element={ <About/>} />
+  <Route path="/users" element={ <Users/>} />
+  <Route path="/contact" element={ <Contact/>} />
+  <Route path="/user/:userId" element={ <User/>} />
+</Routes>
+      </>
 
 
-<h1 >big</h1>
-
-    </>
   )
 }
 
---
+++ src/components/Home.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Col, Container, Row } from 'react-bootstrap'
+
+const Home = () => {
+  return (
+    <Container>
+        <Row className='row-cols-4 justify-content-center'>
+            <Col>
+             <h1>Home</h1>
+            </Col>
+         
+        </Row>
+    </Container>
+  )
+}
+
+export default Home
